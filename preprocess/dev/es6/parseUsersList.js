05628454a2dc15efce976993a644dbb5eed4827e
Michael Hsu (徐承志) <Michael.Hsu@mediatek.com>
2015-04-01 01:33:33 +0800

    count

---
+++ preprocess/dev/es6/parseUsersList.js
@@ -0,0 +1,37 @@
+"use strict";
+
+// node_modules
+import LineByLineReader from "line-by-line";
+import Promise from "bluebird";
+
+// self project
+import $sequelize from "../libs/sequelize";
+
+// Model Schema
+var Users = $sequelize.Users;
+
+var filePath = process.argv[2];
+var lr = new LineByLineReader(filePath, {skipEmptyLines: true });
+
+lr.on("line", (line) => {
+  var [ mid, retweeted_status_mid, uid, retweeted_uid, source, image, text, geo, created_at, deleted_last_seen, permission_denied ] = line.split(",");
+  
+  /**
+  * 初始化 Users 列表
+  *
+  * @param  {string} uid
+  *
+  * @return {int} tweet_counts: 0
+  *
+  * @author Michael Hsu
+  */
+  Promise.resolve()
+  .then(()=>{
+    return Users.findOrCreate({ where:{ uid }, defaults: { tweet_counts: 0 } });
+  })
+  .catch((error)=>{
+    console.log(error);
+  });
+
+});
+
