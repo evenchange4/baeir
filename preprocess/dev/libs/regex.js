d73f49cea25c14ba9c1cb0e421bfe9670fc8a60d
Michael Hsu (徐承志) <Michael.Hsu@mediatek.com>
2015-04-03 18:47:19 +0800

    update topic  regex

--- preprocess/dev/libs/regex.js
+++ preprocess/dev/libs/regex.js
@@ -1,6 +1,19 @@
-export const mention = /@(\S){8,9}：/g;     // example: "@uMLLV3ZCO：", "@uMLLV3ZO："
-export const mentionUkn = /@ukn：/g;        // example: "@ukn："
-export const url = /http(s)*:\/\//g;        // example: "http://", "https://"
-export const geo = /POINT\((.)*\)/g;        // example: "POINT(121.62397 31.10708)"
-export const expression = /\[([^\[\]]*)\]/g;// 表情 ex: [崩潰]
-export const topic = /#(\S)+#/g;            // 話題 ex: #終極一班#
+"use strict";
+
+// example: "@uMLLV3ZCO：", "@uMLLV3ZO："
+export const mention = /@(\S){8,9}：/g;     
+
+// example: "@ukn："
+export const mentionUkn = /@ukn：/g;        
+
+// example: "http://", "https://"
+export const url = /http(s)*:\/\//g;        
+
+// example: "POINT(121.62397 31.10708)"
+export const geo = /POINT\((.)*\)/g;        
+
+// 表情 example: [崩潰]
+export const expression = /\[([^\[\]]*)\]/g;
+
+// 話題 example: #終極一班#
+export const topic = /#([^#]*)#/g;           
