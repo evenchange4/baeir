3ab4e6653478b6c650ddc0bd2233d092ed9f59e7
Michael Hsu (徐承志) <Michael.Hsu@mediatek.com>
2015-04-04 18:10:39 +0800

    update reduce request

--- preprocess/dev/es6/2_isRetweeted.js
+++ preprocess/dev/es6/2_isRetweeted.js
@@ -2,6 +2,7 @@
 
 // node_modules
 import Promise from "bluebird";
+require("babel/register");
 
 // self project modules
 import $sequelize from "../libs/sequelize";
@@ -19,13 +20,62 @@ const Tweets_Tests = $sequelize.Tweets_Tests;
 
 Promise.resolve()
 .then(()=>{
+  
+  /**
+  * 找出所有 retweeted_status_mid 
+  *
+  * @author Michael Hsu
+  */
+
   return Tweets_Trains.findAll({
-    where: {}, 
-    attributes:[ "retweeted_status_mid" ] 
+    where: { }, 
+    attributes:[ "mid", "retweeted_status_mid", "retweeted_counts" ] 
   });
 })
-.map((tweet)=>{
-  let retweeted_status_mid = tweet.dataValues.retweeted_status_mid;
+.then((tweets)=>{
+
+  /**
+  * 塞進 DB 前先做比較處理，減少 requests
+  *
+  * @author Michael Hsu
+  */
+
+  console.log(">> Processing training datasets...");
+  console.log(`number = ${tweets.length}`);
+
+  // Setup two list
+  let midList = [];
+  let retweeted_status_midList = [];
+
+  tweets.forEach((tweet) => {
+    midList.push(tweet.mid);
+    retweeted_status_midList.push(tweet.retweeted_status_mid);
+  });
+
+  // two list intersection
+  let resultsObj = new Map();
+
+  midList.forEach((mid) => {
+    retweeted_status_midList.forEach((retweeted_status_mid) => {
+      if (mid === retweeted_status_mid){
+        if(!resultsObj.has(retweeted_status_mid)) {
+          resultsObj.set(retweeted_status_mid, 1);
+        }
+        else{
+          resultsObj.set(retweeted_status_mid, resultsObj.get(retweeted_status_mid) + 1);
+        }
+      }
+    });
+  });
+
+  // example : { results: { mRsOcOLTlc: 2, mH44qG6iUm: 1 } }
+  let resultList = [];
+  resultsObj.forEach((retweeted_counts, retweeted_status_mid)=>{
+    resultList.push({retweeted_status_mid, retweeted_counts});
+  });
+  return resultList;
+})
+.map((data)=>{
 
   /**
   * 是否被轉錄過 isRetweeted
@@ -39,38 +89,80 @@ Promise.resolve()
   * @author Michael Hsu
   */
 
-  Tweets_Trains.find({ where:{ mid: retweeted_status_mid } })
-  .then((tweet)=>{
-    if (tweet !== null){
-      return tweet.updateAttributes({ 
-        isRetweeted: true,
-        retweeted_counts: tweet.retweeted_counts + 1
-      });
-    }
-  })
+  let {retweeted_status_mid, retweeted_counts} = data
+
+  Tweets_Trains.update(
+    { isRetweeted: true, retweeted_counts }, 
+    { where: {  mid: retweeted_status_mid } }
+  )
   .catch((error)=>{
-    // console.log(error);
+    console.log(error);
   });
 
-
 })
-.catch((error)=>{
-  // console.log(error);
-});
-
-
-// Test
-
-
-Promise.resolve()
 .then(()=>{
+  console.log(">> Training end (async function processing...)");
+})
+.catch((error)=>{
+  console.log(error);
+})
+.then(()=>{
+  
+  /**
+  * 找出所有 retweeted_status_mid 
+  *
+  * @author Michael Hsu
+  */
+
   return Tweets_Tests.findAll({
-    where: {}, 
-    attributes:[ "retweeted_status_mid" ] 
+    where: { }, 
+    attributes:[ "mid", "retweeted_status_mid", "retweeted_counts" ] 
   });
 })
-.map((tweet)=>{
-  let retweeted_status_mid = tweet.dataValues.retweeted_status_mid;
+.then((tweets)=>{
+
+  /**
+  * 塞進 DB 前先做比較處理，減少 requests
+  *
+  * @author Michael Hsu
+  */
+
+  console.log(">> Processing testing datasets...");
+  console.log(`number = ${tweets.length}`);
+
+  // Setup two list
+  let midList = [];
+  let retweeted_status_midList = [];
+
+  tweets.forEach((tweet) => {
+    midList.push(tweet.mid);
+    retweeted_status_midList.push(tweet.retweeted_status_mid);
+  });
+
+  // two list intersection
+  let resultsObj = new Map();
+
+  midList.forEach((mid) => {
+    retweeted_status_midList.forEach((retweeted_status_mid) => {
+      if (mid === retweeted_status_mid){
+        if(!resultsObj.has(retweeted_status_mid)) {
+          resultsObj.set(retweeted_status_mid, 1);
+        }
+        else{
+          resultsObj.set(retweeted_status_mid, resultsObj.get(retweeted_status_mid) + 1);
+        }
+      }
+    });
+  });
+
+  // example : { results: { mRsOcOLTlc: 2, mH44qG6iUm: 1 } }
+  let resultList = [];
+  resultsObj.forEach((retweeted_counts, retweeted_status_mid)=>{
+    resultList.push({retweeted_status_mid, retweeted_counts});
+  });
+  return resultList;
+})
+.map((data)=>{
 
   /**
   * 是否被轉錄過 isRetweeted
@@ -84,21 +176,20 @@ Promise.resolve()
   * @author Michael Hsu
   */
 
-  Tweets_Tests.find({ where:{ mid: retweeted_status_mid } })
-  .then((tweet)=>{
-    if (tweet !== null){
-      return tweet.updateAttributes({ 
-        isRetweeted: true,
-        retweeted_counts: tweet.retweeted_counts + 1
-      });
-    }
-  })
+  let {retweeted_status_mid, retweeted_counts} = data
+
+  Tweets_Tests.update(
+    { isRetweeted: true, retweeted_counts }, 
+    { where: {  mid: retweeted_status_mid } }
+  )
   .catch((error)=>{
-    // console.log(error);
+    console.log(error);
   });
 
-
+})
+.then(()=>{
+  console.log(">> Testing end (async function processing...)");
 })
 .catch((error)=>{
-  // console.log(error);
+  console.log(error);
 });
