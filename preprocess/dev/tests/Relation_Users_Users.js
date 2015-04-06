d7631bfdf6230e3f972b8cc6d1c6a72bbc55ac9e
Michael Hsu (徐承志) <Michael.Hsu@mediatek.com>
2015-04-06 13:14:40 +0800

    update test script

--- preprocess/dev/tests/Relation_Users_Users.js
+++ preprocess/dev/tests/Relation_Users_Users.js
@@ -49,13 +49,13 @@ describe("Relation_Users_Users", (done)=>{
     });
   });
 
-  describe("#record(uzzzzzzz, uxxxxxxx)", ()=>{
+  describe("#record(uxxxxxxx, uzzzzzzz)", ()=>{
     let relation = {};
     before((done)=>{
       Relation_Users_Users.find({ 
         where: {
-          uid1: "uzzzzzzz",
-          uid2: "uxxxxxxx"
+          uid1: "uxxxxxxx",
+          uid2: "uzzzzzzz"
         }
       })
       .then((d)=> {
@@ -66,20 +66,20 @@ describe("Relation_Users_Users", (done)=>{
         console.log(error);
       });
     });
-    it("should return 2 when {uid1_mention_uid2}", (done)=>{
-      assert.equal(relation.uid1_mention_uid2, 2);
+    it("should return 0 when {uid1_mention_uid2}", (done)=>{
+      assert.equal(relation.uid1_mention_uid2, 0);
       done();
     });
-    it("should return 0 when {uid2_mention_uid1}", (done)=>{
-      assert.equal(relation.uid2_mention_uid1, 0);
+    it("should return 2 when {uid2_mention_uid1}", (done)=>{
+      assert.equal(relation.uid2_mention_uid1, 2);
       done();
     });
-    it("should return 2 when {uid1_retweet_uid2}", (done)=>{
-      assert.equal(relation.uid1_retweet_uid2, 2);
+    it("should return 1 when {uid1_retweet_uid2}", (done)=>{
+      assert.equal(relation.uid1_retweet_uid2, 1);
       done();
     });
-    it("should return 1 when {uid2_retweet_uid1}", (done)=>{
-      assert.equal(relation.uid2_retweet_uid1, 1);
+    it("should return 2 when {uid2_retweet_uid1}", (done)=>{
+      assert.equal(relation.uid2_retweet_uid1, 2);
       done();
     });
   });
