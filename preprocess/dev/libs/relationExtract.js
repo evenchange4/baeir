19e8c6f3ae57a76a859df92e57b0089ac12130a6
Michael Hsu (徐承志) <Michael.Hsu@mediatek.com>
2015-05-03 15:20:01 +0800

    JSCS

--- preprocess/dev/libs/relationExtract.js
+++ preprocess/dev/libs/relationExtract.js
@@ -1,8 +1,4 @@
-"use strict";
-
-// node_modules
-import console from "gulp-util";
-// require("babel/register");
+import console from 'gulp-util';
 
 /**
 * Extract usersMap, tweetsMap From relationships, and filter
@@ -22,44 +18,44 @@ export default function relationExtract(relations, userLimit, tweetLimit) {
   let usersMap = new Map();
   let tweetsMap = new Map();
 
-  relations.forEach((relation)=>{
+  relations.forEach((relation)=> {
     // 1. 計算 user 得轉推數量
-    if (usersMap.has(relation.uid)){
-      if(usersMap.get(relation.uid).has(relation.retweeted_status_mid)){
+    if (usersMap.has(relation.uid)) {
+      if (usersMap.get(relation.uid).has(relation.retweeted_status_mid)) {
         usersMap.get(relation.uid).set(
           relation.retweeted_status_mid,
           usersMap.get(relation.uid).get(relation.retweeted_status_mid) + relation.tweet_retweeted_by_user_count
         );
       }
-      else{
+      else {
         usersMap.get(relation.uid).set(
           relation.retweeted_status_mid,
           relation.tweet_retweeted_by_user_count
         );
       }
     }
-    else{
+    else {
       let tempMap = new Map();
       tempMap.set(relation.retweeted_status_mid, relation.tweet_retweeted_by_user_count);
       usersMap.set(relation.uid, tempMap);
     }
 
     // 2. 計算 tweet 轉推數量
-    if (tweetsMap.has(relation.retweeted_status_mid)){
-      if(tweetsMap.get(relation.retweeted_status_mid).has(relation.uid)){
+    if (tweetsMap.has(relation.retweeted_status_mid)) {
+      if (tweetsMap.get(relation.retweeted_status_mid).has(relation.uid)) {
         tweetsMap.get(relation.retweeted_status_mid).set(
           relation.uid,
           tweetsMap.get(relation.retweeted_status_mid).get(relation.uid) + relation.tweet_retweeted_by_user_count
         );
       }
-      else{
+      else {
         tweetsMap.get(relation.retweeted_status_mid).set(
           relation.uid,
           relation.tweet_retweeted_by_user_count
         );
       }
     }
-    else{
+    else {
       let tempMap = new Map();
       tempMap.set(relation.uid, relation.tweet_retweeted_by_user_count);
       tweetsMap.set(relation.retweeted_status_mid, tempMap);
@@ -67,15 +63,15 @@ export default function relationExtract(relations, userLimit, tweetLimit) {
   });
 
   // 3. 篩掉 user Limit
-  usersMap.forEach((value, uid)=>{
-    if (value.size < userLimit){
+  usersMap.forEach((value, uid)=> {
+    if (value.size < userLimit) {
       usersMap.delete(uid);
     }
   });
 
   // 4. 篩掉 tweet Limit
-  tweetsMap.forEach((value, mid)=>{
-    if (value.size < tweetLimit){
+  tweetsMap.forEach((value, mid)=> {
+    if (value.size < tweetLimit) {
       tweetsMap.delete(mid);
     }
   });
