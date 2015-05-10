847dff283dcf39c025c8a1debeebd75c43cacb66
Michael Hsu (徐承志) <Michael.Hsu@mediatek.com>
2015-05-10 17:55:54 +0800

    SVD

--- models/SVD/svd.r
+++ models/SVD/svd.r
@@ -2,18 +2,89 @@
 args<-commandArgs(TRUE)
 
 # ===============================================
+# Functions
+# ===============================================
+
+getSVD = function(n_ignore, matrix_trainSet) {
+  svdresults = svd(matrix_trainSet, nu=min(nrow(matrix_trainSet), ncol(matrix_trainSet)), nv = min(nrow(matrix_trainSet), ncol(matrix_trainSet)), LINPACK = F)
+
+  # ================= 
+  #创建初始值为0矩阵，用来从svd的参数中还原原矩阵  
+  svd2matrix <- matrix(0, nrow=nrow(matrix_trainSet), ncol=ncol(matrix_trainSet))   
+
+  # ================= 
+  #可以尝试略去一些奇异值较小的项  
+  ignorenum <- n_ignore  #设置去掉一些最小奇异值的个数  
+  for (i in 1:(length(svdresults$d) - ignorenum))  
+  {  
+    svd2matrix <- svd2matrix + svdresults$d[i] * as.matrix(svdresults$u[, i]) %*% t(as.matrix(svdresults$v[, i]))   
+  }  
+
+  # ================= 
+  # 求平均绝对偏差，通过平均绝对偏差来衡量整体信息的还原程度  
+  colME <- NULL      
+  for (j in 1:length(svdresults$d))  
+  {  
+    #求每一列的绝对偏差的和  
+    colME <- c(colME, sum(abs(svd2matrix[, j] - matrix_trainSet[, j])))  
+  }  
+
+  # ================= 
+  #根据每一列总和来计算平均绝对偏差  
+  meanME <- sum(colME) / (nrow(matrix_trainSet) * ncol(matrix_trainSet))  
+  print(meanME)
+
+  return (svd2matrix)
+}
+
+getRMSE = function(testSet, matrix_svdResult){
+  n_testSet = length(testSet[,'V1'])
+
+  RMSE_matrix = matrix(0, nrow=n_testSet, ncol=2)
+  colnames(RMSE_matrix) <- c('y', 'y_pred')
+  for (i in 1:n_testSet) {
+    user  = testSet[i,'V1']
+    tweet = testSet[i,'V2']
+    value = testSet[i,'V3']
+    RMSE_matrix[i,'y'] = value
+    RMSE_matrix[i,'y_pred'] = matrix_svdResult[user, tweet]
+  }
+  RMSE <- sqrt(mean((RMSE_matrix[,'y']- RMSE_matrix[,'y_pred'])^2))
+
+  return (RMSE)
+}
+
+# ===============================================
 # Input
 # ===============================================
 
-inputPath = args[1]
-outputPath = args[2]
+trainSetPath = args[1]
+testSetPath = args[2]
 
-m = as.matrix(read.csv(inputPath, sep=",", header=FALSE))
+trainSet = as.matrix(read.csv(trainSetPath, sep=" ", header=FALSE))
+testSet = as.matrix(read.csv(testSetPath, sep=" ", header=FALSE))
 
-s = svd(m)
+n_users = max(trainSet[,'V1'])
+n_tweets = max(trainSet[,'V2'])
+n_trainSet = length(trainSet[,'V1'])
 
-r = s$u %*% diag(s$d) %*% t(s$v)
+matrix_trainSet = matrix(0, nrow=n_users, ncol=n_tweets)
 
-write.table(r,file=outputPath, row.names=FALSE, col.names=FALSE, sep=",", na="")
+for (i in 1:n_trainSet) {
+  user  = trainSet[i,'V1']
+  tweet = trainSet[i,'V2']
+  value = trainSet[i,'V3']
+  matrix_trainSet[user,tweet] = value
+}
 
-print(r)
+matrix_svdResult = getSVD(10, matrix_trainSet)
+
+RMSE = getRMSE(testSet, matrix_svdResult)
+
+print(RMSE)
+
+# r = s$u %*% diag(s$d) %*% t(s$v)
+
+# write.table(svd2matrix,file=outputPath, row.names=FALSE, col.names=FALSE, sep=" ", na="")
+
+# print(r)
